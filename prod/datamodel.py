c67bd85d8609525b2bd69a72d23edf00c132b1ff
Lars Olafsen <larso@redpill-linpro.com>
2020-02-25 10:02:26 +0100

    refactored code. Unbroke load_model

--- prod/datamodel.py
+++ prod/datamodel.py
@@ -1,91 +1,148 @@
 
+from glob import glob
+from joblib import load
+from pathlib import Path
+from voluptuous import Schema, Required, Any, Coerce, Invalid, MultipleInvalid
+
+#
+# Helper class definition of datamodel
+#
+
+
 class datamodel():
+    def __init__( self ):
+        self.metadata = {
+            'bedrooms': {
+                'desc': 'Number of bedrooms', 
+	        'default': 2,
+		'seq': 1,
+		'v': Coerce( int )},
+            'bathrooms': {
+                'desc': 'Number of bathrooms (where .5 accounts for a room with a toilet but no shower)', 
+                'default': 2,
+                'seq': 2,
+                'v': Any( Coerce( float ), Coerce( int ))},
+            'sqft_living': {
+                'desc': 'Square footage of the apartments interior living space', 
+                'default': 2000,
+                'seq': 3,
+                'v': Coerce( int )},                
+            'sqft_lot': {
+                'desc': 'Square footage of the land space', 
+                'default': 20000,
+                'seq': 4,
+                'v': Coerce( int )},
+            'floors': {
+                'desc': 'Number of floors (float or int)', 
+                'default': 2,
+                'seq': 5,
+                'v': Any( Coerce( float ), Coerce( int ))},
+            'waterfront': {
+                'desc': 'Waterfront (0=no, 1=yes)', 
+                'default': 0,
+                'seq': 6,
+                'v': Coerce( int )},
+            'view': {
+                'desc': 'An index from 0 to 4 of how good the view of the property was', 
+                'default': 2,
+                'seq': 7,
+                'v': Coerce( int )},
+            'condition': {
+                'desc': 'An index from 1 to 5 on the condition of the apartment', 
+                'default': 2,
+                'seq': 8,
+                'v': Coerce( int )},
+            'grade': {
+                'desc': 'An index from 1 to 13, where 1-3 falls short of building construction and design, 7 has an average level of construction and design, and 11-13 have a high quality level of construction and design', 
+                'default': 8,
+                'seq': 9,
+                'v': Coerce( int )},
+            'sqft_above': {
+                'desc': 'The square footage of the interior housing space that is above ground level', 
+                'default': 2000,
+                'seq': 10,
+                'v': Coerce( int )},
+            'sqft_basement': {
+                'desc': 'The square footage of the interior housing space that is below ground level', 
+                'default': 0,
+                'seq': 11,
+                'v': Coerce( int )},                
+            'yr_built': {
+                'desc': 'The year the house was initially built',
+                'default': 1990,
+                'seq': 12,
+                'v': Coerce( int )},
+            'yr_renovated': {
+                'desc': 'The year of the house’s last renovation',
+                'default': 0,
+                'seq': 13,
+                'v': Coerce( int )},
+            'zipcode': {
+                'desc': 'What zipcode area the house is in',
+                'default': 98006,
+                'seq': 14,
+                'v': Coerce( int )},
+            'lat': {
+                'desc': 'Lattitude',
+                'default': 47.5,
+                'seq': 15,
+                'v': Coerce( float )},
+            'long': {
+                'desc': 'Longitude',
+                'default': -122.1,
+                'seq': 16,
+                'v': Coerce( float )},
+            'sqft_living15': {
+                'desc': 'The square footage of interior housing living space for the nearest 15 neighbors',
+                'default': 2000,
+                'seq': 17,
+                'v': Coerce( int )},
+            'sqft_lot15': {
+                'desc': 'The square footage of the land lots of the nearest 15 neighbors',
+                'default': 20000,
+                'seq': 18,
+                'v': Coerce( int )},
+            'year': {
+                'desc': 'Year',
+                'default': 2015,
+                'seq': 19,
+                'v': Coerce( int )}
+        }
 
-	self _init_():
-		self.meta = { 'bedrooms': { desc: 'Number of bedrooms', 
-		                            default: 2,
-		                            seq: 1,
-		                            v: Coerce( int )},
-                    'bathrooms': { desc: 'Number of bathrooms (where .5 accounts for a room with a toilet but no shower)', 
-                    default: 2 
-                    seq: 2,
-                    v: Any( Coerce( float ), Coerce( int ))},
-                    'sqft_living': { desc: 'Square footage of the apartments interior living space', 
-                    default: 2000,
-                    seq: 3,
-                    v: Coerce( int )},
 
-                    'sqft_lot': { desc: 'Square footage of the land space', 
-                    default: 20000,
-                    seq: 4,
-                    v: Coerce( int )},
+    def schema( self ):
+        """ Generate a voluptous schema definition """
 
-                    'floors': { desc: 'Number of floors (float or int)', 
-                    default: 2,
-                    seq: 5,
-                    v: Any( Coerce( float ), Coerce( int ))},
-
-                    'waterfront': { desc: 'Waterfront (0=no, 1=yes)', 
-                    default: 0,
-                    seq: 6,
-                    v: Coerce( int )},
-
-                    'view': { desc: 'An index from 0 to 4 of how good the view of the property was', 
-                    default: 2,
-                    seq: 7,
-                    v: Coerce( int )},
-
-                    'condition': { desc: 'An index from 1 to 5 on the condition of the apartment', 
-                    default: 2,
-                    seq: 8,
-                    v: Coerce( int )},
-
-                    'grade': { desc: 'An index from 1 to 13, where 1-3 falls short of building construction and design, 7 has an average level of construction and design, and 11-13 have a high quality level of construction and design', 
-                    default: 8,
-                    seq: 9,
-                    v: Coerce( int )},
-
-                    'sqft_above': { desc: 'The square footage of the interior housing space that is above ground level', 
-                    default: 2000,
-                    seq: 10,
-                    v: Coerce( int )},
-
-                    'sqft_basement': { desc: 'The square footage of the interior housing space that is below ground level', 
-                    default: 0,
-                    seq: 11,
-                    v: Coerce( int )},
-                    'yr_built': [ 'The year the house was initially built', 1990 ],
-                    'yr_renovated': [ 'The year of the house’s last renovation', 0 ],
-                    'zipcode': [ 'What zipcode area the house is in', 98006 ],
-                    'lat': [ 'Lattitude', 47.5 ],
-                    'long': [ 'Longitude', -122.1 ],
-                    'sqft_living15': [ 'The square footage of interior housing living space for the nearest 15 neighbors', 2000 ],
-                    'sqft_lot15': [ 'The square footage of the land lots of the nearest 15 neighbors', 20000 ],
-                    'year': [ 'Year', 2015 ]}
+        defs = {}
+        for key, val in self.metadata.items():
+            defs[ Required( key, default=val[ 'default' ])] = val[ 'v' ]
+            
+        return Schema( defs )
 
 
-        Required( 'bedrooms', default=2 ): Coerce( int ),
-        Required( 'bathrooms', default=2 ): Any( Coerce( float ), Coerce( int )),
-        Required( 'sqft_living', default=2000 ): Coerce( int ),
-        Required( 'sqft_lot', default=20000 ): Coerce( int ),
-        Required( 'floors', default=2 ): Any( Coerce( float ), Coerce( int ) ),
-        Required( 'waterfront', default=0 ): Coerce( int ),
-        Required( 'view', default=2 ): Coerce( int ),
-        Required( 'condition', default=3 ): Coerce( int ),
-        Required( 'grade', default=8 ): Coerce( int ),
-        Required( 'sqft_above', default=2000 ): Coerce( int ),
-        Required( 'sqft_basement', default=0 ): Coerce( int ),
-        Required( 'yr_built', default=1990 ): Coerce( int ),
-        Required( 'yr_renovated', default=0 ): Coerce( int ),
-        Required( 'zipcode', default=98006 ): Coerce( int ),
-        Required( 'lat', default=47.5 ): Coerce( float ),
-        Required( 'long', default=-122.1 ): Coerce( float ),
-        Required( 'sqft_living15', default=2000 ): Coerce( int ),
-        Required( 'sqft_lot15', default=20000 ): Coerce( int ),
-        Required( 'year', default=2015 ): Coerce( int ),
+    def headers( self ):
+        """ Generate array for passing correct parameter sequence to pandas """
+        
+        return sorted( self.metadata, key=self.metadata.get( 'seq' ))
 
-    headers = [ 'bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot',
-                'floors', 'waterfront', 'view', 'condition', 'grade',
-                'sqft_above', 'sqft_basement', 'yr_built',
-                'yr_renovated', 'zipcode', 'lat', 'long',
-                'sqft_living15', 'sqft_lot15', 'year']
+
+    def description( self ):
+        """ Generate structure usable for template engine """
+        
+        desc = {}
+        for key, val in self.metadata.items():
+            desc[ key ] = [ val[ 'desc' ], val[ 'default' ]]
+            
+        return desc
+
+
+    def get_models( self ):
+        """ Return list of model names """
+        
+        return [ Path(i).stem for i in glob( 'models/*.joblib' )]
+
+
+    def load_model( self, name ):
+        """ Load the datamodel that we trained and saved in the Jupyter notebook """
+        
+        return load( Path( 'models', '{}.joblib'.format( name )))
--- prod/predict.py
+++ prod/predict.py
@@ -1,57 +1,40 @@
 
 from flask import Flask, request, jsonify, render_template
-from voluptuous import Schema, Required, Any, Coerce, Invalid, MultipleInvalid
-from joblib import load
-from pathlib import Path
+from voluptuous import Schema, MultipleInvalid
 import pandas as pd
 import sklearn
 
+from datamodel import datamodel
 
 def predict():
     """ Main function. Predict house price given a set of input data and a
         stored model. Return a json struct"""
 
+    dm = datamodel()
+    
     # Get input parameters
-
     input_ok, data = fetch_input()
     if not input_ok:
         return 'Error: %s' % data
 
     # Import the trained model
-
-    model = load_model( 'grad_boosting_regressor' )
+    model = dm.load_model( 'grad_boosting_regressor' )
     if not model:
         return 'Error: Could not load model'
 
     # The model needs the input data in a pandas dataframe. Pass array with headers
-    # ensures correct estmiate
-
-    headers = [ 'bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot',
-                'floors', 'waterfront', 'view', 'condition', 'grade',
-                'sqft_above', 'sqft_basement', 'yr_built',
-                'yr_renovated', 'zipcode', 'lat', 'long',
-                'sqft_living15', 'sqft_lot15', 'year']
+    # ensures correct estmiate    
+    headers = dm.headers()
     input_data = pd.DataFrame([data])
 
     # Now run a prediction given the model and input_data
-
     prediction = model.predict( input_data[headers] )
 
     # Return a json structure containing input and resulting prediction
-
     ret = { 'input': data,
             'prediction': "%.2f" % prediction[0] }
     return ret
 
-
-def get_models():
-    """ Return list of model names """
-    return glob.glob( 'models' )
-
-
-def load_model( name ):
-    """ Load the datamodel that we trained and saved in the Jupyter notebook """
-    return [ Path(i).stem for i in glob.glob( 'models/*.joblib' )]
 
 
 def fetch_input():
@@ -60,27 +43,7 @@ def fetch_input():
         variables are not given. Returns a dict containg input or
         default values for all parameters """
 
-    s = Schema({
-        Required( 'bedrooms', default=2 ): Coerce( int ),
-        Required( 'bathrooms', default=2 ): Any( Coerce( float ), Coerce( int )),
-        Required( 'sqft_living', default=2000 ): Coerce( int ),
-        Required( 'sqft_lot', default=20000 ): Coerce( int ),
-        Required( 'floors', default=2 ): Any( Coerce( float ), Coerce( int ) ),
-        Required( 'waterfront', default=0 ): Coerce( int ),
-        Required( 'view', default=2 ): Coerce( int ),
-        Required( 'condition', default=3 ): Coerce( int ),
-        Required( 'grade', default=8 ): Coerce( int ),
-        Required( 'sqft_above', default=2000 ): Coerce( int ),
-        Required( 'sqft_basement', default=0 ): Coerce( int ),
-        Required( 'yr_built', default=1990 ): Coerce( int ),
-        Required( 'yr_renovated', default=0 ): Coerce( int ),
-        Required( 'zipcode', default=98006 ): Coerce( int ),
-        Required( 'lat', default=47.5 ): Coerce( float ),
-        Required( 'long', default=-122.1 ): Coerce( float ),
-        Required( 'sqft_living15', default=2000 ): Coerce( int ),
-        Required( 'sqft_lot15', default=20000 ): Coerce( int ),
-        Required( 'year', default=2015 ): Coerce( int ),
-    })
+    s = datamodel().schema()
     try:
         return 1, s( dict( request.values ))
     except MultipleInvalid as e: 
@@ -96,34 +59,18 @@ app = Flask( __name__ )
 def liveness():
     return 'Hello there!'
 
+
 @app.route( '/' )
 def serve_html():
 
-    description = { 'bedrooms': [  'Number of bedrooms', 2 ],
-                    'bathrooms': [ 'Number of bathrooms (where .5 accounts for a room with a toilet but no shower)', 2 ],
-                    'sqft_living': [ 'Square footage of the apartments interior living space', 2000 ],
-                    'sqft_lot': [ 'Square footage of the land space', 20000 ],
-                    'floors': [ 'Number of floors (float or int)', 2 ],
-                    'waterfront': [ 'Waterfront (0=no, 1=yes)', 0 ],
-                    'view': [ 'An index from 0 to 4 of how good the view of the property was', 2 ],
-                    'condition': [ 'An index from 1 to 5 on the condition of the apartment', 2 ],
-                    'grade': [ 'An index from 1 to 13, where 1-3 falls short of building construction and design, 7 has an average level of construction and design, and 11-13 have a high quality level of construction and design', 8 ],
-                    'sqft_above': [ 'The square footage of the interior housing space that is above ground level', 2000 ],
-                    'sqft_basement': [ 'The square footage of the interior housing space that is below ground level', 0 ],
-                    'yr_built': [ 'The year the house was initially built', 1990 ],
-                    'yr_renovated': [ 'The year of the house’s last renovation', 0 ],
-                    'zipcode': [ 'What zipcode area the house is in', 98006 ],
-                    'lat': [ 'Lattitude', 47.5 ],
-                    'long': [ 'Longitude', -122.1 ],
-                    'sqft_living15': [ 'The square footage of interior housing living space for the nearest 15 neighbors', 2000 ],
-                    'sqft_lot15': [ 'The square footage of the land lots of the nearest 15 neighbors', 20000 ],
-                    'year': [ 'Year', 2015 ]}
+    dm = datamodel()
+    return render_template( 'predict.html', options = dm.description(), models = dm.get_models() )
 
-    return render_template( 'predict.html', options = description, models = models )
 
 @app.route( '/script.js', methods=[ 'GET' ])
 def serve_script():
     return app.send_static_file( 'script.js' )
+
 
 @app.route( '/predict', methods=[ 'GET', 'POST' ])
 def query_predict():
